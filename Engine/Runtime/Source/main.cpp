8b67310ddf8ad0418fdde30783ee25014e38f0e6
peraul_m <maxime.perault.auditeur@lecnam.net>
2017-11-18 20:55:26 +0100

    GitIgnore modifications

---
+++ Engine/Runtime/Source/main.cpp
@@ -0,0 +1,13 @@
+//
+// Created by maxim on 18/11/2017.
+//
+
+#include <iostream>
+#include <SDL2/include/SDL.h>
+
+int main()
+{
+    std::cout << "Hello World!" << std::endl;
+    SDL_Delay(10);
+    return (0);
+}
