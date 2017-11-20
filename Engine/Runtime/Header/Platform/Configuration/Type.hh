74e80e506d697201da1a4eeba26bafcd68b94229
Aredhele <vincentstehly@hotmail.fr>
2017-11-20 11:42:24 +0100

    Added Platform type wrapper

---
+++ Engine/Runtime/Header/Platform/Configuration/Type.hh
@@ -0,0 +1,46 @@
+/// Copyright (C) 2017-2018
+///
+/// Owners :
+///
+/// Maxime PERAULT
+/// Vincent STEHLY--CALISTO
+///
+/// This program is free software; you can redistribute it and/or modify
+/// it under the terms of the GNU General Public License as published by
+/// the Free Software Foundation; either version 2 of the License, or
+/// (at your option) any later version.
+///
+/// This program is distributed in the hope that it will be useful,
+/// but WITHOUT ANY WARRANTY; without even the implied warranty of
+/// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+/// GNU General Public License for more details.
+///
+/// You should have received a copy of the GNU General Public License along
+/// with this program; if not, write to the Free Software Foundation, Inc.,
+/// 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+/// \file       Type.hh
+/// \date       20/11/2017
+/// \project    Disparity
+/// \package    Platform/Configuration
+/// \author     Vincent STEHLY--CALISTO
+
+#ifndef DISPARITY_TYPE_HPP__
+#define DISPARITY_TYPE_HPP__
+
+typedef unsigned char	    uchar;
+typedef unsigned short	    ushort;
+typedef unsigned int	    uint;
+typedef unsigned long	    ulong;
+
+typedef unsigned char       uint8;
+typedef unsigned short      uint16;
+typedef unsigned int        uint32;
+typedef unsigned long long  uint64;
+
+typedef char                int8;
+typedef short               int16;
+typedef int                 int32;
+typedef long long           int64;
+
+#endif // DISPARITY_TYPE_HPP__
